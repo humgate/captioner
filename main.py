bd2242a00c4509c3cc0ac2f3c0e3c1207c05826f
humgate <humgate@gmail.com>
2024-09-20 11:10:00 +0500

    Move prompts to utils

--- main.py
+++ main.py
@@ -1,24 +1,57 @@
-from openai import OpenAI
+import os
 
-openai_client = OpenAI(
-    base_url="http://192.168.0.195:11434/v1",
-    api_key='ollama'
-)
+import ollama
+import gradio as gr
 
-img_url = "https://civitai.com/images/29857139"
+from util import get_system_prompt
 
-if __name__ == '__main__':
-    response = openai_client.chat.completions.create(
-        model="llava",
-        messages=[
-            {
-                "role": "user",
-                "content": f"please create a caption about 40 words in length for the image: {img_url}"
-            }
-        ],
+
+def get_image_paths(directory):
+    return [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(('.png', '.jpg', '.jpeg'))]
+
+
+image_directory = "/home/humgate/AI/kohya_train/datasets/training_images/Bambolina0"
+images = get_image_paths(image_directory)
+captioning_prompt = get_system_prompt()
+system_prompt = get_system_prompt()
+
+
+def generate_caption(prompt, system, image):
+    result = ollama.generate(
+        model='llava',
+        prompt=prompt,
+        system=system,
+        images=[image],
+        stream=False
+    )['response']
+    return result.replace('"', '')
+
+def show_caption(image_path):
+    # Generate the initial caption
+    return generate_caption(captioning_prompt, system_prompt, image_path)
+
+
+# Gradio interface
+def update_caption(image_path, edited_caption):
+    return image_path, edited_caption
+
+
+with gr.Blocks() as caption_ui:
+    with gr.Row():
+        with gr.Column(scale=4):
+            caption = show_caption(images[0])
+            img = gr.Image(images[0], type="filepath", interactive=True)  # Resized image
+        with gr.Column(scale=6):
+            generated_caption_box = gr.Textbox(caption, lines=3, label="Generated caption")
+            submit_button1 = gr.Button("Generate caption")
+            edited_caption_box = gr.Textbox(caption, interactive=True, lines=3, label="Edited caption")
+            submit_button2 = gr.Button("Save edited caption")
+    submit_button1.click(
+        fn=show_caption,
+        inputs=[img],
+        outputs=[generated_caption_box]
     )
 
-    print(response.choices[0].message.content)
-
-
-
+if __name__ == '__main__':
+    # Launch Gradio app
+    caption_ui.launch()
---
+++ util.py
@@ -0,0 +1,37 @@
+def get_captioning_prompt():
+    return """
+    Please create one caption for image according to guidelines
+    """
+
+
+def get_system_prompt():
+    return """
+    ##Guidelines
+    1. Mention that image is portrait of art doll in caption.
+    2. Identify the portrait type and include it in the caption. Use one of the following:
+    - full-body portrait: Entire body visible from head to toe.
+    - three-quarter body: Body visible from head down to knees.
+    - half-body portrait: Body visible from head down to the waist.
+    - close-up: Only head and upper chest visible.
+    - headshot: Only head visible.
+    - extreme close-up: Only face visible.
+    3. Identify lighting style and include "Rembrandt lighting" or "Daylight lighting" into caption where:
+    - Rembrandt: lighting creates a clearly visible gradient between the brightly lit one side of the object and the darkened other part.
+    - Daylight: background and the subject illuminated evenly from all sides. Lighting does not create high contrasts between dark and light areas.
+    4. The caption should have length about 35 words.
+    """
+
+
+def get_lighting_prompt():
+    return """
+    ## Lighting style identification guide. Use one of the following:
+    - Rembrandt light: a photograph in general in dark tones, with a dark, often almost black background and the object 
+    illuminated unevenly, but mainly on one side. This type of lighting creates a clearly visible gradient between the 
+    brightly lit one side of the object and the darkened other part, thereby creating the illusion of a three-dimensional 
+    image for the viewer and at the same time sharply separating the object from the background. 
+    This type of lighting is often called "Rembrandt light"
+    - Flat light: a photograph in general in light tones, with a light gray or white background and the subject illuminated 
+    evenly from all sides. This type of lighting does not create very bright and very dark areas and high contrasts between
+     them. This type of lighting creates the illusion of lighting as on a cloudy day outside. This type of lighting is often
+      called "Flat light" 
+    """
